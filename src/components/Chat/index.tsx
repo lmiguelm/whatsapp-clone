027b80aa3eab35087dc74b602d16deb32e205e74
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-11-28 16:47:14 -0300

    Cabeçalho de conversa

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-
+import { FiMoreVertical, FiSearch } from 'react-icons/fi';
 import './styles.css';
 
 export default function Chat() {
   return (
     <div id="chat">
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
-      <p>chat</p>
+      <div className="header-container">
+        <div className="header-people">
+          <img src="https://media.discordapp.net/attachments/604432337949950014/781967393114554388/6skneo8tlow51.png" alt="profile" />
+          <div className="info">
+            <strong>Travis Scott</strong>
+            <span>Visto por ultimo hoje as 16:00</span>
+          </div>
+        </div>
+        <div className="header-option">
+          <FiSearch className="icon" size={24} />
+          <FiMoreVertical className="icon" size={24} />
+        </div>
+      </div>
     </div>
   );
 }
