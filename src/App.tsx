b56bbec9ff7a500b0d718933714fed72116898bd
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-12-05 01:27:21 -0300

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { ThemeProvider, useTheme } from './contexts/theme';
+import { ThemeProvider } from './contexts/theme';
+import { CircularProgress, LinearProgress } from '@material-ui/core';
 import axios from 'axios';
 
 import './globalBlack.css';
@@ -34,8 +35,23 @@ function App() {
 
   const [contacts, setContacts] = useState([]);
   const [contactSelected, setContactSelected] = useState<Contact>(initialValue);
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(10);
 
-  const { theme, blackTheme } = useTheme();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   useEffect(() => {
     loadContacts();
@@ -46,16 +62,31 @@ function App() {
     setContacts(res.data);
   }
 
-  return (
-    <ThemeProvider>
-      <div id="page">
-        <div className="page-container">
-          <Aside contacts={contacts} callback={res => setContactSelected(res)} />
-          <Chat contactSelected={contactSelected} />
-        </div>
+  if (loading) {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <CircularProgress style={{ color: '#394045' }} />
       </div>
-    </ThemeProvider>
-  );
+    )
+  } else {
+    return (
+      <ThemeProvider>
+        <div id="page">
+          <div className="page-container">
+            <Aside contacts={contacts} callback={res => setContactSelected(res)} />
+            <Chat contactSelected={contactSelected} />
+          </div>
+        </div>
+      </ThemeProvider>
+    );
+  }
+
+
 }
 
 export default App;
